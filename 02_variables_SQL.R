c758d6e4ae07165a9ca3292d0bea5d94894da388
Jenna Losh <jennal@datacenterresearch.org>
2019-10-16 11:43:16 -0500

    updated CPI

--- 02_variables_SQL.R
+++ 02_variables_SQL.R
@@ -1,8 +1,8 @@
 mycensuskey = "530ce361defc2c476e5b5d5626d224d8354b9b9a"
 
 #CPI numbers, calculated in the nbhd profiles google sheet 'Neighborhood Analysis," documentation in Box
-cpi04 <- 1.311434621
-cpi00 <- 1.438617886
+cpi04 <- 1.338591848
+cpi00 <- 1.468408827
 
 # the current/most recent year - the year the data you're updating represents
 year <- 2018
