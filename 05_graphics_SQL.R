61b271718850a6f3e17a4bf064ed6f9478fae517
Rachel Weinstein <rachelw@datacenterresearch.org>
2020-10-02 14:13:55 -0500

    Qaing

--- 05_graphics_SQL.R
+++ 05_graphics_SQL.R
@@ -164,7 +164,7 @@ chart.demo.allparishes <- ParishDemoforGraphic %>%
         axis.text.x = element_text(size = 12, vjust=1),
         axis.text.y = element_text(size = 12),
         plot.title = element_text(size = 20)) +
-  labs(title = "White, black, Hispanic, and Asian, Metro New Orleans parishes and U.S",
+  labs(title = "White, black, Hispanic, and Asian, Metro New Orleans parishes and U.S.",
        x="",
        y="") 
 
