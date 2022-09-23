e6cf5b21c1ef4cc39c4616ef148f47b2307eee20
Haleigh Tomlin <haleight@datacenterresearch.org>
2022-09-23 13:01:26 -0500

    making sure everything is up to date

---
+++ temp_cleaningup/02_variables.R
@@ -0,0 +1,15 @@
+mycensuskey = "b6844db29933c9dce9e13fa37f1d015281001b95"
+
+#CPI numbers, calculated in the nbhd profiles google sheet 'Neighborhood Analysis," documentation in Box
+cpi04 <- 1.338591848
+cpi00 <- 1.468408827
+
+# the current/most recent year - the year the data you're updating represents
+year <- 2021
+yearPEP <- 2021
+
+year.char <- c("2021")
+yearPEP.char <- c("2021")
+
+
+windowsFonts("Asap" = windowsFont("Asap"))
