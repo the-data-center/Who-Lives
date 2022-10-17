cb1d6b6b0fe4b70ee7be00b113b59b218cd64fff
jennal-datacenter <42979143+jennal-datacenter@users.noreply.github.com>
2022-10-17 13:50:23 -0500

    changed z-score to 1.96; added insignificance notes and footnotes to Rmd

--- indicator expansion drafts/data-pull-expanded.R
+++ indicator expansion drafts/data-pull-expanded.R
@@ -179,9 +179,9 @@ bachnames10 <- c("Total", "TotalMOE", "MaleBach", "MaleBachMOE", "MaleGradProf",
                  "FemaleBachMOE_wht", 
                  "Total_hisp", "TotalMOE_hisp", "MaleBach_hisp", "MaleBachMOE_hisp", "FemaleBach_hisp", 
                  "FemaleBachMOE_hisp")
-Bach10 <- wholivesdatapull(bachvars10, bachnames10, year = 2010)
+Bach10Raw <- wholivesdatapull(bachvars10, bachnames10, year = 2010)
 
-Bach10 <- Bach10 %>%
+Bach10 <- Bach10Raw %>%
   filter(place == "071") %>% 
   transmute(year = 2010,
             pctTotalBach = (MaleBach + FemaleBach + MaleGradProf + FemaleGradProf) / Total,
@@ -191,7 +191,7 @@ Bach10 <- Bach10 %>%
   pivot_longer(cols = pctTotalBach:pctHispBach, values_to = "val") %>% 
   select(year, val, name)
 
-Bach10MOE <- Bach10 %>%
+Bach10MOE <- Bach10Raw %>%
   filter(place == "071") %>% 
   mutate(year = 2010,
             pctTotalBach = (MaleBach + FemaleBach + MaleGradProf + FemaleGradProf) / Total,
@@ -386,9 +386,9 @@ povnames10 <- c("Total", "TotalMOE", "BelowPov", "BelowPovMOE",
                 "Total_asian", "TotalMOE_asian", "BelowPov_asian", "BelowPovMOE_asian",
                 "Total_wht", "TotalMOE_wht", "BelowPov_wht", "BelowPovMOE_wht",
                 "Total_hisp", "TotalMOE_hisp", "BelowPov_hisp", "BelowPovMOE_hisp")
-pov10 <- wholivesdatapull(povvars10, povnames10, year = 2010)
+pov10Raw <- wholivesdatapull(povvars10, povnames10, year = 2010)
 
-pov10 <- pov10 %>%
+pov10 <- pov10Raw %>%
   filter(place == "071") %>% 
   transmute(year = 2010,
             pctTotalpov = BelowPov / Total,
@@ -398,7 +398,7 @@ pov10 <- pov10 %>%
   pivot_longer(cols = pctTotalpov:pctHisppov, values_to = "val") %>% 
   select(year, val, name)
 
-pov10MOE <- pov10 %>%
+pov10MOE <- pov10Raw %>%
   filter(place == "071") %>% 
   transmute(year = 2010,
             pctTotalpov = BelowPov / Total,
